3e6b32337ad72ddcbc7b27f55b0de6215df08bb4
dimkouv <dimkouv@protonmail.com>
2020-12-22 17:18:41 +0200

    22/12 - refactor

--- 22/p2.py
+++ 22/p2.py
@@ -1,39 +1,26 @@
 with open("input.txt") as f:
-    lines = f.read().replace("\n", " ").replace("Player 2:", "#").replace("Player 1: ", "")
-    p1, p2 = lines.strip().split("  # ")
-    p1 = [int(c) for c in p1.split(" ")]
-    p2 = [int(c) for c in p2.split(" ")]
+    deck1, deck2 = list(map(lambda x: list(map(int, x.split(":\n")[1].split("\n"))), f.read().strip().split("\n\n")))
 
-
-def recursive_combat(p1, p2, depth=1):
+def recursive_combat(deck1, deck2):
     history = set()
-    k = (tuple(p1), tuple(p2))
 
-    while p1 and p2:
+    while deck1 and deck2:
+        k = (tuple(deck1), tuple(deck2))
         if k in history:
             return 1
+
         history.add(k)
+        card1, card2 = deck1.pop(0), deck2.pop(0)
 
-        c1 = p1.pop(0)
-        c2 = p2.pop(0)
-
-        if c1 <= len(p1) and c2 <= len(p2):
-            winner = recursive_combat(p1[:c1][:], p2[:c2][:], depth=depth+1)
+        if card1 <= len(deck1) and card2 <= len(deck2):
+            winner = recursive_combat(deck1[:card1], deck2[:card2])
         else:
-            winner = 1 if c1 > c2 else 2
+            winner = 1 if card1 > card2 else 2
 
-        if winner == 1:
-            p1 += [c1, c2]
-        else:
-            p2 += [c2, c1]
+        deck1 += [card1, card2] if winner == 1 else []
+        deck2 += [card2, card1] if winner == 2 else []
 
-        k = (tuple(p1), tuple(p2))
+    return 1 if deck1 else 2
 
-    if depth == 1:
-        return p1, p2
-    else:
-        return 1 if p1 else 2
-
-
-P1, P2 = recursive_combat(p1, p2)
-print(sum([(i+1)*v for i, v in enumerate(reversed(P1+P2))]))
+recursive_combat(deck1, deck2)
+print(sum([(i+1) * v for i, v in enumerate(reversed(deck1 + deck2))]))
